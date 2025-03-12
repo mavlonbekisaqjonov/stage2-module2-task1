ee6aa0430113538bc3d81df51eecd568c2b64fea
Mavlonbek Isaqjonov <mavlonbekisaqjonov222@gmail.com>
2025-03-12 07:53:25 +0500

    servlets are added and fixed

--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -8,15 +8,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/users") // Alternative to web.xml mapping
 public class GetUsersServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<User> users = (List<User>) Warehouse.getInstance().getUsers();
+        // Convert Set<User> to List<User>
+        List<User> users = new ArrayList<>(Warehouse.getInstance().getUsers());
 
         request.setAttribute("users", users);
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
+
 }
