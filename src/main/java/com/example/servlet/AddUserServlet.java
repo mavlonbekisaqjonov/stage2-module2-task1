762256970f7c2186244eb786f723fcc38c60349e
Mavlonbek Isaqjonov <mavlonbekisaqjonov222@gmail.com>
2025-03-12 07:50:55 +0500

    servlets are added

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,21 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = new User(request.getParameter("firstName"), request.getParameter("lastName"));
+        Warehouse.getInstance().addUser(user);
+
+        request.setAttribute("user", user);
+        request.getRequestDispatcher("/add.jsp").forward(request, response);
+    }
 }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,22 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
-    //write your code here!
+import com.example.Warehouse;
+import com.example.User;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/users") // Alternative to web.xml mapping
+public class GetUsersServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<User> users = (List<User>) Warehouse.getInstance().getUsers();
+
+        request.setAttribute("users", users);
+        request.getRequestDispatcher("/users.jsp").forward(request, response);
+    }
 }
